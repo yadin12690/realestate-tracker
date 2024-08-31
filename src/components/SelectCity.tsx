9d611692c8fbf7e4355161f5a1ebe6fc7de80b88
Yadin Yafe <yadin258@yahoo.com>
2024-08-31 18:40:46 +0300

    fetchig cities list and display in select

--- src/components/SelectCity.tsx
+++ src/components/SelectCity.tsx
@@ -15,17 +15,17 @@ const SelectCity = () => {
     if (isError) return <div>Error loading cities</div>;
 
     return (
-        <div className="w-full max-w-xs">
-            <label htmlFor="beautifulSelect" className="block text-lg font-medium text-gray-700 mb-2">
-                Choose an Option
+        <div className="w-full max-w-xs rtl:p-0">
+            <label htmlFor="beautifulSelect" className="block text-lg font-medium text-gray-700 mb-2 rtl:pr-4">
+                בחרו עיר על מנת שנתחיל
             </label>
             <div className="relative">
                 <select
                     id="beautifulSelect"
                     className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded-lg shadow leading-tight focus:outline-none focus:shadow-outline"
                 >
-                    <option value="">Please select</option>
-                    {data && data.map((city: { id: string; name: string }) => (
+                    <option value="">בחרו עיר</option>
+                    {data && data.record.map((city: { id: string; name: string }) => (
                         <option key={city.id} value={city.id}>
                             {city.name}
                         </option>
